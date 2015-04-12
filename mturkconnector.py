623d5be0c5006ce6081a641eda227f166031b6bc
JW-Vinay <vinay.ramkrishnan@gmail.com>
2015-04-12 18:11:10 -0400

    added code for creating hits programmatically

---
+++ mturkconnector.py
@@ -0,0 +1,64 @@
+from boto.mturk.connection import MTurkConnection
+from boto.mturk.question import QuestionContent,Question,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent
+
+SANDBOX = True 
+HIT_URL = "https://codingthecrowd.com/class/vramkris/Assignment_4/game.html"
+NUMBER_OF_HITS = 1  # Number of different HITs posted for this task
+NUMBER_OF_ASSIGNMENTS = 1  # Number of tasks that DIFFERENT workers will be able to take for each HIT
+LIFETIME = 60 * 7  # How long that the task will stay visible if not taken by a worker (in seconds)
+REWARD = 0.1  # Base payment value for completing the task (in dollars)
+DURATION = 60*2  # How long the worker will be able to work on a single task (in seconds)
+APPROVAL_DELAY = 60*60*2  # How long after the task is completed will the worker be automatically paid if not manually approved (in seconds)
+
+
+# HIT title (as it will appear on the public listing)
+TITLE = 'Earn $0.10/question'
+# Description of the HIT that workers will see when deciding to accept it or not
+DESCRIPTION = 'Go through the answer choices. Give us the right one and we reward you.'
+# Search terms for the HIT posting
+KEYWORDS = ['choices', 'multiple choice', 'question', 'twitter']
+
+
+# Your Amazon Web Services Access Key (private)
+AWS_ACCESS_KEY = 'AKIAJMXQ3GZJOW2XDITQ' # <-- TODO: Enter your access key here
+# Your Amazon Web Services Secret Key (private)
+AWS_SECRET_KEY = 'HIjdRm0sOx5hdp8rFOwIOUo4NKrmHQ8dEtMXt7hl' # <-- TODO: Enter your private key here
+
+def postHitAndSetReviewIntervals(question, answers):
+	ratings =[('Very Bad','-2'),
+         ('Bad','-1'),
+         ('Not bad','0'),
+         ('Good','1'),
+         ('Very Good','1')]
+	createdHit = createHits(question, ratings);
+	print "Testing now"
+
+def createHits(question, answers):
+	if SANDBOX:
+		mturk_url = 'mechanicalturk.sandbox.amazonaws.com'
+		preview_url = 'https://workersandbox.mturk.com/mturk/preview?groupId='
+	else:
+		mturk_url = 'mechanicalturk.amazonaws.com'
+		preview_url = 'https://mturk.com/mturk/preview?groupId='
+
+	# q = ExternalQuestion(external_url=HIT_URL, frame_height=800)
+
+	overview  = Overview()
+	overview.append_field('Title', 'We need the crowd!!')
+	overview.append(FormattedContent('<a href="http://programthecrowd.com/">Visit us here</a>'))
+	questionContent = QuestionContent()
+	questionContent.append_field('Title', question);
+	answerChoices = SelectionAnswer(min=1, max=1, style='checkbox', selections=answers, type='text', other=False)
+	q = Question(identifier='Help', content=questionContent, answer_spec=AnswerSpecification(answerChoices), is_required=True)
+	questionForm = QuestionForm();
+	questionForm.append(overview)
+	questionForm.append(q)
+	conn = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, host=mturk_url)
+	for i in range(0, NUMBER_OF_HITS):
+		create_hit_rs = conn.create_hit(questions=questionForm, lifetime=LIFETIME, max_assignments=NUMBER_OF_ASSIGNMENTS, title=TITLE, keywords=KEYWORDS, reward=REWARD, duration=DURATION, approval_delay=APPROVAL_DELAY, annotation=DESCRIPTION)
+		print(preview_url + create_hit_rs[0].HITTypeId)
+		print("HIT ID: " + create_hit_rs[0].HITId)
+
+
+
+postHitAndSetReviewIntervals("how good are you???")		
