f0b99327a9e3a2354469deee585774decd2295de
JW-Vinay <vinay.ramkrishnan@gmail.com>
2015-04-12 23:15:59 -0400

    combined create hit and reviewable hits

--- get_hits.py
+++ get_hits.py
@@ -1,22 +1,11 @@
 from boto.mturk.connection import MTurkConnection
-import ConfigParser
- 
-HOST = 'mechanicalturk.sandbox.amazonaws.com'
+import settings
+from threading import Timer
 
-config = ConfigParser.ConfigParser()
-config.read("config.ini")
-
-def readConfigurations():
-    dict = {};
-    sections = config.sections()
-    for section in sections:
-        options = config.options(section)
-        for option in options:
-            dict[option] = config.get(section, option)
-    return dict;    
-
-def get_all_reviewable_hits(mtc):
+def get_all_reviewable_hits(hitIdList, mtc):
     page_size = 10
+    print hitIdList
+    print mtc
     hits = mtc.get_reviewable_hits(page_size=page_size)
 
     print "Total results to fetch %s " % hits.TotalNumResults
@@ -33,24 +22,19 @@ def get_all_reviewable_hits(mtc):
         print "Request hits page %i" % pn
         temp_hits = mtc.get_reviewable_hits(page_size=page_size,page_number=pn)
         hits.extend(temp_hits)
+    for hit in hits:
+        print "--------------------"
+        print "Hit ID %s" %hit.HITId
+        assignments = mtc.get_assignments(hit.HITId)
+        for assignment in assignments:
+            print "Answers of the worker %s" % assignment.WorkerId
+
+            for question_form_answer in assignment.answers[0]:
+                print question_form_answer.fields;
+                # for key, value in question_form_answer.fields:
+                #     print "%s: %s" % (key,value)
+                print "--------------------"
+    # create timer to  invoke self after 10 seconds only if results are not yet available
     return hits
- 
-params = readConfigurations() 
-mtc = MTurkConnection(aws_access_key_id=params['aws_access_key'],
-                      aws_secret_access_key=params['aws_secret_key'],
-                      host=HOST)
- 
-hits = get_all_reviewable_hits(mtc)
 
-for hit in hits:
-    print "--------------------"
-    print "Hit ID %s" %hit.HITId
-    assignments = mtc.get_assignments(hit.HITId)
-    for assignment in assignments:
-        print "Answers of the worker %s" % assignment.WorkerId
 
-        for question_form_answer in assignment.answers[0]:
-            print question_form_answer.fields;
-            # for key, value in question_form_answer.fields:
-            #     print "%s: %s" % (key,value)
-            print "--------------------"
--- mturkconnector.py
+++ mturkconnector.py
@@ -1,7 +1,8 @@
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import QuestionContent,Question,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent
-import ConfigParser
 from threading import Timer
+import settings 
+import get_hits
 
 SANDBOX = True 
 NUMBER_OF_HITS = 1  # Number of different HITs posted for this task
@@ -11,7 +12,7 @@ REWARD = 0.1  # Base payment value for completing the task (in dollars)
 DURATION = 60*2  # How long the worker will be able to work on a single task (in seconds)
 APPROVAL_DELAY = 60*60*2  # How long after the task is completed will the worker be automatically paid if not manually approved (in seconds)
 
-
+conn = None
 # HIT title (as it will appear on the public listing)
 TITLE = 'Earn $0.10/question'
 # Description of the HIT that workers will see when deciding to accept it or not
@@ -21,27 +22,15 @@ KEYWORDS = ['choices', 'multiple choice', 'question', 'twitter']
 
 
 
-config = ConfigParser.ConfigParser()
-config.read("config.ini")
-
-def readConfigurations():
-	dict = {};
-	sections = config.sections()
-	for section in sections:
-		options = config.options(section)
-		for option in options:
-			dict[option] = config.get(section, option)
-	return dict;		
-
 def postHitAndSetReviewIntervals(question, answers):
 	ratings =[('Very Bad','-2'),
          ('Bad','-1'),
          ('Not bad','0'),
          ('Good','1'),
          ('Very Good','1')]
-	createdHit = createHits(question, ratings)
-	Timer(10, getAllReviewableHits).start()
-	#print "Testing now"
+	hitIdList = createHits(question, ratings)
+	# get_hits.get_all_reviewable_hits(hitIdList, conn)
+	Timer(10, get_hits.get_all_reviewable_hits, [hitIdList, conn]).start()
 
 def createHits(question, answers):
 	if SANDBOX:
@@ -51,8 +40,7 @@ def createHits(question, answers):
 		mturk_url = 'mechanicalturk.amazonaws.com'
 		preview_url = 'https://mturk.com/mturk/preview?groupId='
 
-	# q = ExternalQuestion(external_url=HIT_URL, frame_height=800)
-
+	#Create Hit Form Structure	
 	overview  = Overview()
 	overview.append_field('Title', 'We need the crowd!!')
 	overview.append(FormattedContent('<a href="http://programthecrowd.com/">Visit us here</a>'))
@@ -63,13 +51,18 @@ def createHits(question, answers):
 	questionForm = QuestionForm();
 	questionForm.append(overview)
 	questionForm.append(q)
+	hitIdList = []
+	global conn
 	conn = MTurkConnection(aws_access_key_id=params['aws_access_key'], aws_secret_access_key=params['aws_secret_key'], host=mturk_url)
+	#For Loop to create and post hits
 	for i in range(0, NUMBER_OF_HITS):
 		create_hit_rs = conn.create_hit(questions=questionForm, lifetime=LIFETIME, max_assignments=NUMBER_OF_ASSIGNMENTS, title=TITLE, keywords=KEYWORDS, reward=REWARD, duration=DURATION, approval_delay=APPROVAL_DELAY, annotation=DESCRIPTION)
 		print(preview_url + create_hit_rs[0].HITTypeId)
 		print("HIT ID: " + create_hit_rs[0].HITId)
+		hitIdList.append(create_hit_rs[0].HITId);
 
+	return hitIdList
 
-params = readConfigurations()
-print params
+params = settings.readConfigurations()
+# print params
 postHitAndSetReviewIntervals("how good are you???", answers=None)		
